4babad6e76e214d3d62699829b7500aed49e4b57
unknown <tiago.matias92@gmail.com>
2018-07-23 19:58:54 +0000

    Tests

---
+++ RefactorHat.Tests/HatUserTestCase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RefactorHat.Tests
+{
+    [TestFixture]
+    class HatUserTestCase
+    {
+        private HatUser User;
+
+        [SetUp]
+        public void Init()
+        {
+            User = new HatUser("testUser");
+        }
+
+        [Test]
+        public void PutOnRedHat_UserPutOnRedHat_GreenHatIsZero()
+        {
+            User.PutOnRedHat();
+            var timerValue = float.Parse(User.GetGreenHatTimerValue(), CultureInfo.InvariantCulture);
+            Assert.AreEqual(timerValue, 0);
+        }
+
+        [Test]
+        public void PutOnRedHat_UserPutOnRedHat_RefactorHatIsZero()
+        {
+            User.PutOnRedHat();
+            // Parse Doesnt Recognize "."
+            var timerValue = float.Parse(User.GetRefactorHatTimerValue(), CultureInfo.InvariantCulture);
+            Assert.AreEqual(timerValue, 0);
+        }
+
+        [Test]
+        public void PutOnRedHat_UserPutOnRedHat_RedHatIsNotZero()
+        {
+            User.PutOnRedHat();
+            // Parse Doesnt Recognize "."
+            var timerValue = float.Parse(User.GetRedHatTimerValue(), CultureInfo.InvariantCulture);
+            Assert.IsTrue(timerValue > 0);
+        }
+    }
+}
